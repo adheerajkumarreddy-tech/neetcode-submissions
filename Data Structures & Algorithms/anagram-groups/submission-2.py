24c436fc1ad344c880d4b2a22ae63c3cd4384565
adheerajkumarreddy-tech <adheerajkumarreddy@gmail.com>
2026-06-01 21:50:04 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        hashMap={}
+        for word in strs:
+            tup = self.createTuple(word)
+            if (tup in hashMap):
+                hashMap[tup].append(word)
+            else:
+                hashMap[tup]=[word]
+        ans=[]
+        for val in hashMap.values():
+            ans.append(val)
+        return ans
+    def createTuple(self,word):
+        lis=[0]*26;
+        for ch in word:
+            lis[ord(ch)-ord('a')]+=1
+        return tuple(lis)
